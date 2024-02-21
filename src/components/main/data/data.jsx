894bfb953d997d545f0b2683a5fad17fea6fc101
Dada Khalandar <114235328+sheikhfarook@users.noreply.github.com>
2024-02-21 16:03:45 +0530

    create data

---
+++ src/components/main/data/data.jsx
@@ -0,0 +1,42 @@
+import egg from "../../../assets/desktop/image-transform.jpg ";
+import juiceGlass from "../../../assets/desktop/image-stand-out.jpg";
+import cherry from "../../../assets/desktop/image-graphic-design.jpg";
+import orange from "../../../assets/desktop/image-photography.jpg";
+import egg1 from "../../../assets/mobile/image-transform.jpg ";
+import juiceGlass1 from "../../../assets/mobile/image-stand-out.jpg";
+import cherry1 from "../../../assets/mobile/image-graphic-design.jpg";
+import orange1 from "../../../assets/mobile/image-photography.jpg";
+const data = [
+  {
+    id: 1,
+    title: "Transform your brand",
+    para: "We are a full-service creative agency specializing in helping brands grow fast. Engage your clients through compelling visuals that do most of the marketing for you.",
+    more: "Learn More",
+    profile: egg,
+    profile1: egg1,
+  },
+  {
+    id: 2,
+    title: "Stand out to the right audience",
+    para: "Using a collaborative formula of designers, researchers, photographers, videographers, and copywriters, we’ll build and extend your brand in digital places.",
+    more: "Learn More",
+    profile: juiceGlass,
+    profile1: juiceGlass1,
+  },
+  {
+    id: 3,
+    title: "Graphic Design",
+    para: "Great design makes you memorable. We deliver artwork that underscores your brand message and captures potential clients’ attention.",
+    more: "Learn More",
+    profile: cherry,
+    profile1: cherry1,
+  },
+  {
+    id: 4,
+    title: "Photography",
+    para: "Increase your credibility by getting the most stunning, high-quality photos that improve your business image.",
+    more: "Learn More",
+    profile: orange,
+    profile1: orange1,
+  },
+];
