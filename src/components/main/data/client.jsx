3477b5d65a5328a76dff3c4fba582f25733262a0
Dada Khalandar <114235328+sheikhfarook@users.noreply.github.com>
2024-02-22 11:58:07 +0530

    i create new data

---
+++ src/components/main/data/client.jsx
@@ -0,0 +1,27 @@
+import emily from "../../../assets/image-emily.jpg";
+import thomas from "../../../assets/image-thomas.jpg";
+import jennie from "../../../assets/image-jennie.jpg";
+
+export const ClientData = [
+  {
+    id: 1,
+    profile: emily,
+    para: "We put our trust in Sunnyside and they delivered, making sure our needs were met and deadlines were always hit.",
+    name: "Emily R.",
+    roll: "Marketing Director",
+  },
+  {
+    id: 2,
+    profile: thomas,
+    para: "Sunnyside’s enthusiasm coupled with their keen interest in our brand’s success made it a satisfying and enjoyable experience.",
+    name: "Thomas S.",
+    roll: "Chief Operating Officer",
+  },
+  {
+    id: 3,
+    profile: jennie,
+    para: "Incredible end result! Our sales increased over 400% when we worked with Sunnyside. Highly recommended!",
+    name: "Jennie F.",
+    roll: "Business Owner",
+  },
+];
